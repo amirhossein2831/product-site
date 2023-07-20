711f6c28a3317e139a6860a7fa8099cf6ff27ae3
Amirhossein <Amirmemool12@gmail.com>
2023-07-20 21:58:53 +0330

    :hammer: chore: add get post and resolve method to the Router class

--- App/php/Router.php
+++ App/php/Router.php
@@ -2,7 +2,31 @@
 
 namespace app;
 
-class ProductController
+class Router
 {
+    public array $getRout = [];
+    public array $postRout = [];
 
+    public function get($url,$fn){
+        $this->getRout[$url] = $fn;
+    }
+    public function post($url,$fn){
+        $this->postRout[$url] = $fn;
+    }
+    public function resolve(): void
+    {
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method === 'GET') {
+            $fn = $this->getRout[$currentUrl] ?? null;
+        }else
+            $fn = $this->postRout[$currentUrl] ?? null;
+        if ($fn) {
+            var_dump($fn);
+        }else
+            echo "Page Not Found";
+
+
+    }
 }
