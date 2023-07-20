4b3d17dbf67f0ba0e68fff26aeadc02c863a3c83
Amirhossein <Amirmemool12@gmail.com>
2023-07-21 00:06:18 +0330

    :card_file_box: chore: implement the getProducts method in DB class

--- App/php/DBConnection/DBConnection.php
+++ App/php/DBConnection/DBConnection.php
@@ -6,11 +6,20 @@ use PDO;
 
 class DBConnection {
     public PDO $connection;
+    public array $products;
     public function __construct(){
         $this->connection = new PDO("mysql:host=localhost;port=3306;dbname=product", 'root', '13821382');
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->products = [];
     }
+    public function getProducts($search = ''): void {
+        if ($search) {
+            $statement = $this->connection->prepare("SELECT * FROM product WHERE title LIKE '$search' ORDER BY date_create DESC ");
+        }else
+            $statement = $this->connection->prepare("SELECT * FROM product ORDER BY date_create DESC ");
 
+        $statement->execute();
+        $this->products = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
 
 }
