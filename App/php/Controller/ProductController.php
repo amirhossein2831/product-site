af9b8c58eb1df2f46240938a0b3ae4a113c54f80
Amirhossein <Amirmemool12@gmail.com>
2023-07-21 00:01:25 +0330

    :hammer: chore: create renderView method in ProductController

--- App/php/Controller/ProductController.php
+++ App/php/Controller/ProductController.php
@@ -4,7 +4,22 @@ namespace app\Controller;
 
 class ProductController
 {
-    public function index(){
-
+    public function index(): void {
+        $this->renderView('product/index');
+    }
+    public function create(): void {
+        $this->renderView('product/create');
+    }
+    public function update(): void {
+        $this->renderView('product/update');
+    }
+    public function delete(): void {
+        $this->renderView('product/delete');
+    }
+    public function renderView($view): void {      //viewName like index create ...
+        ob_start();
+        include_once __DIR__ . "/../views/$view.php";
+        $content = ob_get_clean();
+        include_once __DIR__ . '/../views/_layout.php';
     }
 }
