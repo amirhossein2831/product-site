be6270af0c2a39ec06a56522f246f458e8ae54c7
Amirhossein <Amirmemool12@gmail.com>
2023-07-21 00:03:05 +0330

    :hammer: chore: initail the getRout and postRout in index.php

--- App/php/Router.php
+++ App/php/Router.php
@@ -2,31 +2,30 @@
 
 namespace app;
 
-class Router
-{
+class Router {
     public array $getRout = [];
     public array $postRout = [];
 
-    public function get($url,$fn){
+    public function get($url, $fn): void {
         $this->getRout[$url] = $fn;
     }
-    public function post($url,$fn){
+
+    public function post($url, $fn): void {
         $this->postRout[$url] = $fn;
     }
-    public function resolve(): void
-    {
+
+    public function resolve(): void {
         $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
             $fn = $this->getRout[$currentUrl] ?? null;
-        }else
+        } else
             $fn = $this->postRout[$currentUrl] ?? null;
         if ($fn) {
-            var_dump($fn);
-        }else
+            call_user_func($fn);
+        } else
             echo "Page Not Found";
-
-
     }
+
 }
--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use app\Controller\ProductController;
+use app\Router;
+
+$router = new Router();
+initialRouter($router);
+$router->resolve();
+
+function initialRouter(Router $router): void {
+    $productController = new ProductController();
+    $router->get('/', [$productController, 'index']);
+    $router->get('/product', [$productController, 'index']);
+    $router->get('/product/create', [$productController, 'create']);
+    $router->get('/product/update', [$productController, 'update']);
+    $router->post('/product/create', [$productController, 'create']);
+    $router->post('/product/update', [$productController, 'update']);
+    $router->post('/product/delete', [$productController, 'delete']);
+}
