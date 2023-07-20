183872d5dbbf327024b0e176ed59d368a8709e80
Amirhossein <Amirmemool12@gmail.com>
2023-07-21 00:05:15 +0330

    :card_file_box: chore: set a connection to DB

--- App/php/DBConnection/DBConnection.php
+++ App/php/DBConnection/DBConnection.php
@@ -5,7 +5,12 @@ namespace app\DBConnection;
 use PDO;
 
 class DBConnection {
-   
+    public PDO $connection;
+    public function __construct(){
+        $this->connection = new PDO("mysql:host=localhost;port=3306;dbname=product", 'root', '13821382');
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->products = [];
+    }
 
 
 }
