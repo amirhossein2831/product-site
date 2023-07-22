a12bb8e6d42a627a14f883aa2e9b8de3d180e09f
unknown <amirmemool12@gmail.com>
2023-07-22 20:39:01 +0430

    :lipstick: style: add main.css and normalize.css to the project

--- App/php/views/_layout.php
+++ App/php/views/_layout.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Product</title>
+    <link rel="stylesheet" href="./Css/main.css">
+    <link rel="stylesheet" href="./Css/normalize.css">
     <link rel="stylesheet" href="./Css/productsStyle.css">
 </head>
 <body>
