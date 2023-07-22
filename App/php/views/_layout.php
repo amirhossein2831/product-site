39a67deff6bc61c8da97a91248f7e04fdd5179ae
unknown <amirmemool12@gmail.com>
2023-07-22 20:35:12 +0430

    :lipstick: style: add lin to css style in _layout

--- App/php/views/_layout.php
+++ App/php/views/_layout.php
@@ -6,9 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Product</title>
+    <link rel="stylesheet" href="./Css/productsStyle.css">
 </head>
 <body>
     <?php echo $content; ?>
-
 </body>
 </html>
