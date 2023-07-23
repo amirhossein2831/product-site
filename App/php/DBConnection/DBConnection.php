b934a18aa651bef0e50a73661b906caa64d07169
unknown <amirmemool12@gmail.com>
2023-07-23 15:41:47 +0430

    :card_file_box: chore: setProduct method to add product to Db

--- App/php/DBConnection/DBConnection.php
+++ App/php/DBConnection/DBConnection.php
@@ -2,6 +2,7 @@
 
 namespace app\DBConnection;
 
+use app\Models\Product;
 use PDO;
 
 class DBConnection {
@@ -9,6 +10,7 @@ class DBConnection {
     public function __construct(){
         $this->connection = new PDO("mysql:host=localhost;port=3306;dbname=product", 'root', '13821382');
         $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
     }
     public function getProducts($search = ''): false|array  {
         if ($search) {
@@ -17,5 +19,13 @@ class DBConnection {
             $statement = $this->connection->prepare("SELECT * FROM product ORDER BY create_date ASC ");
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function setProduct(Product $product): bool
+    {
+        $date = date('Y-m-d H:i:s');
+        $statement = $this->connection->prepare("INSERT INTO product (title,description,price,picture,create_date)
+                                                         VALUES('$product->title','$product->description','$product->price','$product->imagePath','$date')");
+        return $statement->execute();
     }
 }
