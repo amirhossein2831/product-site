250d7f3c91e4e4f84ca353d687db3ff2c085b63b
unknown <amirmemool12@gmail.com>
2023-07-23 21:26:06 +0430

    :hammer: chore: implement update method in Product Model

--- App/php/Models/Product.php
+++ App/php/Models/Product.php
@@ -19,7 +19,7 @@ class Product
         $this->title = $productDate['title'];
         $this->description = $productDate['description'] ?? '';
         $this->price =(float) $productDate['price'];
-        $this->imagePath = null;
+        $this->imagePath =$productDate['picture'] ??  null;
         $this->imageFile = $imageFile ?? null;
     }
 
@@ -29,20 +29,22 @@ class Product
         $Db->setProduct($this);
     }
 
+    public function update(DBConnection $Db): void
+    {
+        $this->saveFile();
+        $Db->updateProduct($this);
+    }
+
     public function saveFile(): void
     {
-        if (!is_null($this->imageFile)) {
+        if (!is_null($this->imageFile) && $this->imageFile['tmp_name']!=='') {
             $filePath = $this->imageFile['tmp_name'];
             $originalFilename = $this->imageFile['name'];
             $fileExtension = pathinfo($originalFilename, PATHINFO_EXTENSION);
             $uniqueFilename = uniqid('file_', true) . '.' . $fileExtension;
-
             $targetFile = './resource/image/'. $uniqueFilename;
             $this->imagePath = 'resource/image/'. $uniqueFilename;
-            if (move_uploaded_file($filePath,$targetFile)) {
-                echo "sss";
-            }
-            echo "file";
+            move_uploaded_file($filePath,$targetFile);
         }
     }
 
