8ce1f0082b51e7cc900d9cabde4f93f3020bf5e8
unknown <amirmemool12@gmail.com>
2023-07-23 15:40:10 +0430

    :hammer: chore: change the path of the link

--- App/php/views/_layout.php
+++ App/php/views/_layout.php
@@ -1,3 +1,7 @@
+<?php
+$pathInfo = $_SERVER['PATH_INFO'] ?? '';
+$path = $pathInfo ? (strlen($pathInfo) > 9 ? './../../' : './') : './';
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,9 +10,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Product</title>
-    <link rel="stylesheet" href="./Css/main.css">
-    <link rel="stylesheet" href="./Css/normalize.css">
-    <link rel="stylesheet" href="./Css/productsStyle.css">
+    <link rel="stylesheet" href="<?php echo $path?>Css/main.css">
+    <link rel="stylesheet" href="<?php echo $path?>Css/normalize.css">
+    <link rel="stylesheet" href="<?php echo $path?>Css/productsStyle.css">
 </head>
 <body>
     <?php echo $content; ?>
