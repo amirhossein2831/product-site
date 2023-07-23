40b4bbd6a17ede50bea00f01d313a9338dfbb5b7
unknown <amirmemool12@gmail.com>
2023-07-23 15:58:33 +0430

    :hammer: improve: improve the js file code

--- public/js/validation.js
+++ public/js/validation.js
@@ -3,19 +3,23 @@ const titleInput = document.querySelector(".create-title-input");
 const priceInput = document.querySelector(".create-price-input");
 
 submit.addEventListener("click",evt => {
-    if (titleInput.value.trim() === '' && priceInput.value.trim() === '') {
-        evt.preventDefault();
-        titleInput.classList.add("empty");
-        titleInput.placeholder = "Title can't be empty";
-        priceInput.classList.add("empty");
-        priceInput.placeholder = "Price can't be empty";
-    }else if(titleInput.value.trim() === '' ){
-        evt.preventDefault();
-        titleInput.classList.add("empty");
-        titleInput.placeholder = "Title can't be empty";
-    }else if(priceInput.value.trim() === '' ){
-        evt.preventDefault();
-        priceInput.classList.add("empty");
-        priceInput.placeholder = "Price can't be empty";
-    }
+    if(titleInput.value.trim() === '' )
+        addError(titleInput, evt, "Title");
+    else
+        removeError(titleInput);
+
+    if(priceInput.value.trim() === '' )
+        addError(priceInput, evt, "Price");
+    else
+        removeError(priceInput);
 })
+
+function addError(button,evt,buttonName) {
+    evt.preventDefault();
+    button.classList.add("empty");
+    button.placeholder = buttonName + " can't be empty";
+}
+function removeError(inputElement) {
+    inputElement.classList.remove("empty");
+    inputElement.placeholder = "";
+}
