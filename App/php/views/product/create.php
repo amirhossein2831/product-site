3473c78726439cbf70b4331ed2774a2b919c95f3
unknown <amirmemool12@gmail.com>
2023-07-23 15:42:52 +0430

    :lipstick: feat: implement the create product page

--- App/php/views/product/create.php
+++ App/php/views/product/create.php
@@ -1,1 +1,38 @@
-<h1>create Views</h1>
+<article>
+    <h1>Create New Product</h1>
+    <form class="create" action="" method="post" enctype="multipart/form-data">
+        <div class="first-container">
+            <div class="second-container">
+                <label for="">Product Image</label>
+            </div>
+            <input id="upload-file" type="file" name="image">
+        </div>
+
+        <div class="first-container">
+            <div class="second-container">
+                <label for="">Product Title</label>
+            </div>
+            <input class="create-title-input" type="text" name="title">
+        </div>
+
+        <div class="first-container description-container">
+            <div class="second-container">
+                <label for="">Product Description</label>
+            </div>
+            <input type="text" name="description">
+        </div>
+        <div class="first-container">
+            <div class="second-container">
+                <label for="">Product Price</label>
+            </div>
+            <input class="create-price-input" type="text" name="price">
+        </div>
+        <button type="submit" class="submit-button">Submit</button>
+    </form>
+</article>
+<script src="./../../js/validation.js"></script>
+
+
+
+
+
